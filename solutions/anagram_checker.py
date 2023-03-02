5d58b84884ece6f7af002315b7fcf7acd7c5b1a5
Casper Burik <cburik@users.noreply.github.com>
2023-03-02 17:55:58 +0100

    Change requirements

--- solutions/anagram_checker.py
+++ solutions/anagram_checker.py
@@ -5,7 +5,7 @@ def count_string(string: str) -> dict:
     return counts
 
 
-def anagram_checker(string_one: str, string_two: str) -> bool:    
+def anagram_checker(string_one: str, string_two: str) -> bool:
     counts_one = count_string(string_one)
     counts_two = count_string(string_two)
     return counts_one == counts_two
